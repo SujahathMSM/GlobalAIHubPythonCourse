2ea025f43522d33a10b6dbd08cdae917793f39a0
SujahathMSM <78421570+SujahathMSM@users.noreply.github.com>
2021-02-02 21:14:44 +0530

    Update Hw_D02_01.py

--- Homeworks/Hw_D02_01.py
+++ Homeworks/Hw_D02_01.py
@@ -1,5 +1,5 @@
-My_list=[1,4,9,16,25,36,10,20,30,40,50,60]
-New_List=[]
-first_Half=My_list[0:6]
-Second_Half=My_list[6:]
-print(Second_Half+first_Half)
+My_list=[1,4,9,16,25,36,10,20,30,40,50,60]  #Creating a list with some Numbers
+New_List=[]  # Creating empty list
+first_Half=My_list[0:6]  #Extracting the first half elements from  My_list
+Second_Half=My_list[6:]  #Extracting the second half elements from  My_list
+print(Second_Half+first_Half)  # Swapping first and second half elements 
