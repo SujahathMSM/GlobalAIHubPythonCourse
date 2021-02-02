93debb451a891f345737856f36ccc2d8144b1871
SujahathMSM <78421570+SujahathMSM@users.noreply.github.com>
2021-02-02 21:16:33 +0530

    Update Hw_D02_02.py

--- Homeworks/Hw_D02_02.py
+++ Homeworks/Hw_D02_02.py
@@ -1,4 +1,4 @@
-n=int(input('Enter a number: '))
-l=[i for i in range(n+1) if i%2==0]
+n=int(input('Enter a number: '))  # get an integer from user
+l=[i for i in range(n+1) if i%2==0]  # finding the even numbers and appending to a list
 for j in l:
-    print(j)
+    print(j)                  #Extraxcting the even numbers 
